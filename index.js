a71bbfbb459dcea64a7011e6421936d2a6aeec55
EnzoJF24 <enzofulcheri@gmail.com>
2022-11-20 19:04:13 -0300

    intro-uno-js: primer ejercicio

---
+++ index.js
@@ -0,0 +1,13 @@
+/*let ciudad="junin";
+let numRand=45;
+ciudad=numRand;
+console.log("ciudad: " + ciudad);
+console.log("numero Random: " + numRand);*/     //esto esta comentado para que solo se imprima lo de abajo
+
+//--------------------------------------
+
+const nombre = "Enzo";
+const apellido = "Fulcheri";
+nombreCompleto = `${nombre} ${apellido}`;
+
+console.log(nombreCompleto);
