3a25c2a8cec918c948617a9021773cb185a48451
EnzoJF24 <enzofulcheri@gmail.com>
2022-12-27 12:48:25 -0300

    ejercicio 3 incompleto

--- index.js
+++ index.js
@@ -1,47 +1,23 @@
-/*let ciudad="junin";
-let numRand=45;
-ciudad=numRand
-console.log("ciudad: " + ciudad)
-console.log("numero Random:" + numRand)     //esto hay que comentarlo para que solo se imprima lo de abajo
+import heroes from './heroes.js';
 
 
-const nombre = "Enzo";
-const apellido = "Fulcheri";
-nombreCompleto = `${nombre} ${apellido}`;
-
-console.log(nombreCompleto)*/
-
-//--------------------------------------
-
-const celular ={
-    marca: "motorola",
-    color: "azul",
-    precio: 19000,
-    anio_creacion: 2020
+function getHeroeByIdAsync (id){
+    return new Promise((buscar, error)=>{
+        if ((id <= heroes.length) && (id >= 0)){
+            buscar(id)
+        }
+        else{
+            //error("error")
+        }
+    });
 }
 
-console.log(celular.precio);
-celular.stock=true;
-celular.anio_creacion=2017;
-delete celular.marca;
+getHeroeByIdAsync().then(function(id){
+    (id) => heroes.find((heroe) => heroe.id == id)})
+    
+.catch(function(mensaje){
+    console.log(mensaje)});
 
-for (const [key, value] of Object.entries(celular)) {
-    console.log(`${key}: ${value}`);
-}
+console.log(getHeroeByIdAsync(2))
 
-let array = [1,2,3,4];
-let array2 = [...array,5]
-console.log(array)
-console.log(array2)
-
-const person = {
-    name: 'Jose',
-    age: 26,
-    hobbies: ['chess', 'running', 'basket']
-}
-
-let {name: nombre, age: edad, hobbies} = person;
-console.log(nombre)
-console.log(edad)
-console.log(hobbies)
 
