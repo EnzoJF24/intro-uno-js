e1e7858ac714710b43559a4264fb617238083a8a
EnzoJF24 <enzofulcheri@gmail.com>
2023-01-20 12:03:56 -0300

    ejercicio 3 completo y algunas dudas

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 import heroes from './heroes.js';
 
+//punto 1
+/*const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
 
-function getHeroeByIdAsync (id){
-    return new Promise((buscar, error)=>{
-        if ((id <= heroes.length) && (id >= 0)){
-            buscar(id)
+const getHeroeByIdAsync = (id) => {
+    const heroe = getHeroeById(id)
+    return new Promise((resolve, reject)=>{
+        if (heroe === undefined){
+            reject("reject")
         }
-        else{
-            //error("error")
-        }
+        resolve(heroe.name)
     });
 }
 
-getHeroeByIdAsync().then(function(id){
-    (id) => heroes.find((heroe) => heroe.id == id)})
-    
-.catch(function(mensaje){
-    console.log(mensaje)});
+getHeroeByIdAsync().then(getHeroeById())  
+.catch(function(mensaje){console.log(mensaje)});
 
-console.log(getHeroeByIdAsync(2))
+console.log(getHeroeByIdAsync(8))*/
 
+/*consultas punto 1: 
+    +por que si le paso un id valido me imprime el nombre (como si funcionara bien) y debajo "reject" (como si estuviese mal)
+    +por que si le paso un id invalido me imprime tira un error*/
 
+//punto 2
+/*fetch('https://api.github.com/users/manishmshiva')
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.log(error))*/
+
+/* consultas punto 2:
+    +en este caso transforme la respuesta en formato json, pero como se si viene en ese formato o en oro?*/
+
+//punto 3
+async function getName(){
+    let manishmshiva = await fetch ('https://api.github.com/users/manishmshiva')
+    console.log(manishmshiva)
+}
+
+getName()
+
+/* consultas punto 3:
+    +no se como hacer para que me imprima el atributo name como pide el ejercicio, creo que no esta bien hecho este punto*/
